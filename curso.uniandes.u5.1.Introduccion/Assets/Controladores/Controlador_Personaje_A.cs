ae324b0f22fe45d4e5fcec4a7d14a5d3b76b9e3b
Fernando Navarro <fnp.formacion@gmail.com>
2016-12-06 22:21:57 +0100

    Incluir en el personaje y en un objeto estático una prueba de movimiento
    Signed-off-by: Fernando Navarro <fnp.formacion@gmail.com>

---
+++ curso.uniandes.u5.1.Introduccion/Assets/Controladores/Controlador_Personaje_A.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controlador_Personaje_A : MonoBehaviour {
+	public float Velocidad = 0.5f;
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate () {
+		Vector2 m = new Vector2 (Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		rb.AddForce (m * Velocidad);
+	}
+}
