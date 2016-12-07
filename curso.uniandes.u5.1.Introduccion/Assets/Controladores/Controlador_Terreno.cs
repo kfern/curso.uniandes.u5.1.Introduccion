7f6f491bd3d92102e2abc0e552eb137fdfb2556b
Fernando Navarro <fnp.formacion@gmail.com>
2016-12-07 01:27:28 +0100

    Se añade un controlador para el terreno que permita saber cuando un objeto está dentro o fuera del escenario
    Signed-off-by: Fernando Navarro <fnp.formacion@gmail.com>

---
+++ curso.uniandes.u5.1.Introduccion/Assets/Controladores/Controlador_Terreno.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controlador_Terreno : MonoBehaviour {
+
+	void OnTriggerExit2D(Collider2D collider)
+	{
+		Debug.Log("Algo se ha salido del terreno. Si es el personaje está en el mar y debería perder vida o su equivalente."); 
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		Debug.Log ("Algo ha entrado. Al iniciar el juego se lanza el evento PARA CADA ELEMENTO QUE COLISIONE CON EL TERRENO!!. Si estaba perdiendo vida ha de dejar de perderla."); 
+	}
+}
+ 
