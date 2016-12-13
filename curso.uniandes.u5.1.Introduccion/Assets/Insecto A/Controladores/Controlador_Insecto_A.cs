af4cbc8f840f67de4423f3ee61d186df2696f212
Fernando Navarro <fnp.formacion@gmail.com>
2016-12-13 22:33:16 +0100

    Resolver que el Insecto A apunte a un target de forma suave y reorganizar los archivos de Insecto A
    Signed-off-by: Fernando Navarro <fnp.formacion@gmail.com>

---
+++ curso.uniandes.u5.1.Introduccion/Assets/Insecto A/Controladores/Controlador_Insecto_A.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controlador_Insecto_A : MonoBehaviour {
+	public GameObject target;
+	public float speed = .5f;
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//http://answers.unity3d.com/questions/650460/rotating-a-2d-sprite-to-face-a-target-on-a-single.html
+		Vector3 vectorToTarget = target.transform.position - transform.position;
+		float angle = (Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg) - 90;
+		Quaternion q = Quaternion.AngleAxis (angle, Vector3.forward);
+		transform.rotation = Quaternion.Slerp (transform.rotation, q, Time.deltaTime * speed);
+	}
+		
+}
